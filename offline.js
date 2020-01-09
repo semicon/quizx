1959d0f4a2881edfa33c8e4cc47ace82d0a635ff
semicon <1210@sts.ac.th>
2020-01-09 08:19:05 +0700

    quizx
    quizx

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1578529790,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/btnchoice-sheet0.png",
+		"images/animal-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/logo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"questions.txt"
+	]
+}
